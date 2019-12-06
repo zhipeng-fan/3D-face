193b7040fe0e2e7b472b87e9a59d82537d8bca8b
zhipeng-fan <zhipeng.fan95@gmail.com>
2019-12-05 23:22:54 -0500

    Torch implementation finished!

--- BFM.py
+++ BFM.py
@@ -4,7 +4,6 @@ import numpy as np
 from scipy.io import loadmat, savemat
 from array import array
 
-# define facemodel for reconstruction
 class BFM():
 	"""
 	This is a numpy implementation of BFM model
@@ -13,10 +12,10 @@ class BFM():
 	def __init__(self):
 		model_path = './BFM/BFM_model_front.mat'
 		model = loadmat(model_path)
-		self.meanshape = model['meanshape'] # mean face shape 
+		self.meanshape = model['meanshape'].T # mean face shape 
 		self.idBase = model['idBase'] # identity basis
 		self.exBase = model['exBase'] # expression basis
-		self.meantex = model['meantex'] # mean face texture
+		self.meantex = model['meantex'].T # mean face texture
 		self.texBase = model['texBase'] # texture basis
 		self.point_buf = model['point_buf'] # adjacent face index for each vertex, starts from 1 (only used for calculating face normal)
 		self.tri = model['tri'] # vertex index for each triangle face, starts from 1
@@ -28,106 +27,43 @@ class BFM_torch(nn.Module):
 	Used in the DNN model, comes with gradient support
 	"""
 	def __init__(self, BFM_MODEL):
-		self.meanshape = torch.register_buffer("meanshape", BFM_MODEL.meanshape)
-		self.idBase = torch.register_buffer("idBase", BFM_MODEL.idBase)
-		self.exBase = torch.register_buffer("exBase", BFM_MODEL.exBase)
-		self.meantex = torch.register_buffer("meantex", BFM_MODEL.meantex)
-		self.texBase = torch.register_buffer('texBase', BFM_MODEL.texBase)
+		super(BFM_torch, self).__init__()
+		model_path = './BFM/BFM_model_front.mat'
+		model = loadmat(model_path)
+		# [107127, 1]
+		self.register_buffer("meanshape", torch.tensor(model['meanshape'].T, dtype=torch.float32))
+		# [107127, 80]
+		self.register_buffer("idBase", torch.tensor(model['idBase'], dtype=torch.float32))
+		# [107127, 64]
+		self.register_buffer("exBase", torch.tensor(model['exBase'], dtype=torch.float32))
+		# [107127, 1]
+		self.register_buffer("meantex", torch.tensor(model['meantex'].T, dtype=torch.float32))
+		# [107121, 80]
+		self.register_buffer('texBase', torch.tensor(model['texBase'], dtype=torch.float32))
 		
 
 	def get_shape(self, id_param, ex_param):
 		"""
 		Perform shape assembly from index parameter and expression parameter
+		id_param: [bs, 80]
+		ex_param: [bs, 64]
+		return: [bs, 107127, 1]
 		"""
-		return self.meanshape
+		assert id_param.shape[0] == ex_param.shape[0]
+		bs = id_param.shape[0]
 
-# ############################## BFM MODEL PREPROCESSING ##########################
-# load expression basis
-def LoadExpBasis():
-	n_vertex = 53215
-	Expbin = open('BFM/Exp_Pca.bin','rb')
-	exp_dim = array('i')
-	exp_dim.fromfile(Expbin,1)
-	expMU = array('f')
-	expPC = array('f')
-	expMU.fromfile(Expbin,3*n_vertex)
-	expPC.fromfile(Expbin,3*exp_dim[0]*n_vertex)
+		id_base = self.idBase[None,:,:].expand(bs,-1,-1)
+		ex_base = self.exBase[None,:,:].expand(bs,-1,-1)
 
-	expPC = np.array(expPC)
-	expPC = np.reshape(expPC,[exp_dim[0],-1])
-	expPC = np.transpose(expPC)
+		return self.meanshape+torch.bmm(id_base,id_param[:,:,None])+torch.bmm(ex_base,ex_param[:,:,None])
 
-	expEV = np.loadtxt('BFM/std_exp.txt')
-
-	return expPC,expEV
-
-# transfer original BFM09 to our face model
-def transferBFM09():
-	original_BFM = loadmat('BFM/01_MorphableModel.mat')
-	shapePC = original_BFM['shapePC'] # shape basis
-	shapeEV = original_BFM['shapeEV'] # corresponding eigen value
-	shapeMU = original_BFM['shapeMU'] # mean face
-	texPC = original_BFM['texPC'] # texture basis
-	texEV = original_BFM['texEV'] # eigen value
-	texMU = original_BFM['texMU'] # mean texture
-
-	expPC,expEV = LoadExpBasis()
-
-	# transfer BFM09 to our face model
-
-	idBase = shapePC*np.reshape(shapeEV,[-1,199])
-	idBase = idBase/1e5 # unify the scale to decimeter
-	idBase = idBase[:,:80] # use only first 80 basis
-
-	exBase = expPC*np.reshape(expEV,[-1,79])
-	exBase = exBase/1e5 # unify the scale to decimeter
-	exBase = exBase[:,:64] # use only first 64 basis
-
-	texBase = texPC*np.reshape(texEV,[-1,199])
-	texBase = texBase[:,:80] # use only first 80 basis
-
-	# our face model is cropped align face landmarks which contains only 35709 vertex.
-	# original BFM09 contains 53490 vertex, and expression basis provided by JuYong contains 53215 vertex.
-	# thus we select corresponding vertex to get our face model.
-
-	index_exp = loadmat('BFM/BFM_front_idx.mat')
-	index_exp = index_exp['idx'].astype(np.int32) - 1 #starts from 0 (to 53215)
-
-	index_shape = loadmat('BFM/BFM_exp_idx.mat')
-	index_shape = index_shape['trimIndex'].astype(np.int32) - 1 #starts from 0 (to 53490)
-	index_shape = index_shape[index_exp]
-
-
-	idBase = np.reshape(idBase,[-1,3,80])
-	idBase = idBase[index_shape,:,:]
-	idBase = np.reshape(idBase,[-1,80])
-
-	texBase = np.reshape(texBase,[-1,3,80])
-	texBase = texBase[index_shape,:,:]
-	texBase = np.reshape(texBase,[-1,80])
-
-	exBase = np.reshape(exBase,[-1,3,64])
-	exBase = exBase[index_exp,:,:]
-	exBase = np.reshape(exBase,[-1,64])
-
-	meanshape = np.reshape(shapeMU,[-1,3])/1e5
-	meanshape = meanshape[index_shape,:]
-	meanshape = np.reshape(meanshape,[1,-1])
-
-	meantex = np.reshape(texMU,[-1,3])
-	meantex = meantex[index_shape,:]
-	meantex = np.reshape(meantex,[1,-1])
-
-	# other info contains triangles, region used for computing photometric loss,
-	# region used for skin texture regularization, and 68 landmarks index etc.
-	other_info = loadmat('BFM/facemodel_info.mat')
-	frontmask2_idx = other_info['frontmask2_idx']
-	skinmask = other_info['skinmask']
-	keypoints = other_info['keypoints']
-	point_buf = other_info['point_buf']
-	tri = other_info['tri']
-	tri_mask2 = other_info['tri_mask2']
-
-	# save our face model
-	savemat('BFM/BFM_model_front.mat',{'meanshape':meanshape,'meantex':meantex,'idBase':idBase,'exBase':exBase,'texBase':texBase,'tri':tri,'point_buf':point_buf,'tri_mask2':tri_mask2\
-		,'keypoints':keypoints,'frontmask2_idx':frontmask2_idx,'skinmask':skinmask})
+	def get_texture(self, tex_param):
+		"""
+		Perform texture assembly from texture parameter
+		tex_param: [bs, 80]
+		return: [bs, 107127, 1]
+		"""
+		bs = tex_param.shape[0]
+		tex_base = self.texBase[None,:,:].expand(bs,-1,-1)
+		
+		return self.meantex+torch.bmm(tex_base,tex_param[:,:,None])
--- draft.py
+++ draft.py
@@ -2,9 +2,11 @@ import BFM
 import os
 import trimesh
 import numpy as np
+import utils
+import torch
 
-if os.path.isfile("./BFM/BFM_model_front.mat"):
-    BFM.transferBFM09()
+if not os.path.isfile("./BFM/BFM_model_front.mat"):
+    utils.transferBFM09()
 
 bfm_face_model = BFM.BFM()
 
@@ -14,27 +16,41 @@ bfm_face_model = BFM.BFM()
 
 # mesh.show()
 
-type(bfm_face_model.idBase)
-
-def morph_face(bfm_model, id_param=None, ex_param=None, tex_param=None):
-    if id_param is None:
-        id_param = np.random.randn(80, 1)
-    if ex_param is None:
-        ex_param = np.random.randn(64, 1)
-    if tex_param is None:
-        tex_param = np.random.randn(80,1)
-
-    vertices = bfm_face_model.meanshape.T+np.dot(bfm_face_model.idBase, id_param)#+np.dot(bfm_face_model.exBase, ex_param)
-    colors = np.clip(bfm_face_model.meantex.T+np.matmul(bfm_face_model.texBase, tex_param), 0, 255)
-    mesh = trimesh.Trimesh(vertices=vertices.reshape(-1,3), 
-                            faces=bfm_face_model.tri.reshape(-1,3)-1, 
-                            vertex_colors=colors.reshape(-1,3))
-
-    mesh.show()
 
 
-morph_face(bfm_face_model)
-print (bfm_face_model.texBase.shape)
-print (bfm_face_model.idBase.shape)
-print (bfm_face_model.exBase.shape)
+# def morph_face(bfm_model, id_param=None, ex_param=None, tex_param=None):
+#     if id_param is None:
+#         id_param = np.random.randn(80, 1)
+#     if ex_param is None:
+#         ex_param = np.random.randn(64, 1)
+#     if tex_param is None:
+#         tex_param = np.random.randn(80,1)
 
+#     vertices = bfm_face_model.meanshape+np.dot(bfm_face_model.idBase, id_param)#+np.dot(bfm_face_model.exBase, ex_param)
+#     colors = np.clip(bfm_face_model.meantex+np.matmul(bfm_face_model.texBase, tex_param), 0, 255)
+#     mesh = trimesh.Trimesh(vertices=vertices.reshape(-1,3), 
+#                             faces=bfm_face_model.tri.reshape(-1,3)-1, 
+#                             vertex_colors=colors.reshape(-1,3))
+
+#     mesh.show()
+
+
+# morph_face(bfm_face_model)
+
+# print (bfm_face_model.meanshape.shape)
+# print (bfm_face_model.texBase.shape)
+# print (bfm_face_model.exBase.shape)
+# print (bfm_face_model.meantex.shape)
+# print (bfm_face_model.idBase.shape)
+
+face_model = BFM.BFM_torch(bfm_face_model)
+shape = face_model.get_shape(torch.randn(1,80), torch.zeros(1,64))[0]
+albedo = face_model.get_texture(torch.randn(1,80))[0]
+
+print (albedo.shape)
+print ((albedo.numpy().reshape(-1,3)))
+
+mesh = trimesh.Trimesh(vertices=shape.numpy().reshape(-1,3), 
+                       faces=bfm_face_model.tri.reshape(-1,3)-1, 
+                       vertex_colors=np.clip(albedo.numpy().reshape(-1,3)/255.,0,1))
+mesh.show()
